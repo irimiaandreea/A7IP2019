2b19f19eaf8d45a30eec1b363b8a469ac7bcc8c2
stefanbotez <43352665+stefanbotez@users.noreply.github.com>
2019-04-14 10:21:29 +0300

    Add files via upload

---
+++ microservices2/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'RON', 4.21, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'EUR', 'RON', 4.76, 0);
