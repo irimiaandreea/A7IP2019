6a145ad3f9e5c185e37524d90dea2b6dfcb277ab
Tifir <maria_vladut52@yahoo.com>
2019-04-20 14:55:35 +0300

    am modificat Registerul

--- Microservices/sender_user_management/src/main/java/com/senderUserManagement/register/RegisterController.java
+++ Microservices/sender_user_management/src/main/java/com/senderUserManagement/register/RegisterController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senderUserManagement.model.Profiles;
+import com.senderUserManagement.model.RegisterDetails;
 import com.senderUserManagement.model.Users;
 
 
@@ -16,7 +18,7 @@ public class RegisterController
 	
 	
 	@PostMapping("/register")
-	public String registerUser(@RequestBody Users user)
+	public String registerUser(@RequestBody RegisterDetails user)
 	{
 		return userLoginDataService.addUser(user);
 	}
--- Microservices/sender_user_management/src/main/java/com/senderUserManagement/register/RegisterService.java
+++ Microservices/sender_user_management/src/main/java/com/senderUserManagement/register/RegisterService.java
@@ -3,14 +3,17 @@ package com.senderUserManagement.register;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.senderUserManagement.model.Profiles;
+import com.senderUserManagement.model.RegisterDetails;
 import com.senderUserManagement.model.Users;
+import com.senderUserManagement.repositories.ProfilesRepository;
 import com.senderUserManagement.repositories.UsersRepository;
 
 
 @Service
 public class RegisterService {
 
-	private boolean isPasswordStrong(Users user)
+	private boolean isPasswordStrong(RegisterDetails user)
 	{
         if(user.getPassword().length()<=20 && user.getPassword().length()>=4)
         {
@@ -23,16 +26,24 @@ public class RegisterService {
 	
 	@Autowired
 	private UsersRepository usersRepository;
+	@Autowired
+	private ProfilesRepository profilesRepository;
 	
-	public String addUser(Users user) 
-	{
-		if(  isPasswordStrong(user) && 
-				!usersRepository.existsById(user.getUsername()))
+
+	public String addUser(RegisterDetails newUser) 
+	{	
+	
+		if(  isPasswordStrong(newUser) && 
+				!usersRepository.existsById(newUser.getUsername()))
 		{
+			Users user = new Users(newUser.getUsername(),newUser.getPassword());
+			Profiles userProfile = new Profiles(newUser.getUsername(),newUser.getEmail(),newUser.getPhone_number()
+					,null,null,null,null,null,null,null);
+				profilesRepository.save(userProfile);
 				usersRepository.save(user);
 				return "Succes";
 		}
 		return "Invalid data";
 	}
-
+	// email / username / phoneNr | NULL -> country / profile photo /adresses
 }
