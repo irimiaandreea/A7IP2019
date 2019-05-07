7314d282403e2a318fbb9c4b198ae09c973efdde
zornet <40139779+zornet@users.noreply.github.com>
2019-05-07 15:26:48 +0300

    Am rezolvat microserviciul de forgot password

--- account_management/src/main/java/com/accountManagement/recoverPassword/RecoverPasswordController.java
+++ account_management/src/main/java/com/accountManagement/recoverPassword/RecoverPasswordController.java
@@ -1,27 +1,34 @@
 package com.accountManagement.recoverPassword;
 
-import javax.mail.MessagingException;
 
+import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.accountManagement.model.Users;
+
+import java.util.Random;
 
 @RestController
 public class RecoverPasswordController {
 	
 	@Autowired
-	private  ResetPasswordService smtpMailSender;
+	private ResetPasswordService smtpMailSender;
+	private PasswordGenerator newPassword;
 
-	@RequestMapping("/passwordRecovery")
-	public void sendMail() throws MessagingException {
+	@RequestMapping("/passwordRecovery/{email}")
+	public void sendMail(@PathVariable String email) throws MessagingException {
+		String password = "";
+		Random r = new Random();
+		int low = 6;
+		int high = 15;
+		int result = r.nextInt(high-low) + low;
+		password=newPassword.generatePassword(result);
+		smtpMailSender.send(email, "Your iUber password", "Parola ta este "+password ,password);		
 		
-		String password="";
-		
-		
-		
-		smtpMailSender.send("zornet.samp@gmail.com", "Your new iUber password", "Parola ta schimbata este: ");
 		
 	}
 	
--- account_management/src/main/java/com/accountManagement/recoverPassword/ResetPasswordService.java
+++ account_management/src/main/java/com/accountManagement/recoverPassword/ResetPasswordService.java
@@ -6,16 +6,33 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
+
+import com.accountManagement.exceptions.UnknownMatchException;
+import com.accountManagement.model.Users;
+import com.accountManagement.repositories.UsersRepository;
 
 @Component
 public class ResetPasswordService {
 	
 	@Autowired
-	private JavaMailSender javaMailSender;	
+	private JavaMailSender javaMailSender;
+	@Autowired
+	private UsersRepository userRepository;
 	
-	public void send(String to, String subject, String body) throws MessagingException {
-		
+	
+	public void send(String to, String subject, String body,String newPassword) throws MessagingException {
+		if(!userRepository.existsById(to)) throw new UnknownMatchException ("Adresa de email nu este inregistrata");
+		{
+		    Users user=new Users();
+			user.setPassword(newPassword);
+			user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+			user.setEmail(to);
+			userRepository.save(user);
+			
+			
+		}
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper;
 		
