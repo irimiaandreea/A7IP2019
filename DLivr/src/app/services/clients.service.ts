11001e8d683a06c333e15841879be285a5675225
irimiaandreea <irimia.andreea6@yahoo.com>
2019-05-13 04:57:30 +0300

    My Pack Driver Functionality

--- DLivr/src/app/services/clients.service.ts
+++ DLivr/src/app/services/clients.service.ts
@@ -12,18 +12,20 @@ export class ClientsService {
   loggedIn = false;
   accessToken: String = '';
   email: String = '';
-
+  
   constructor(private http: HttpClient, public alertController: AlertController, private router: Router) { }
 
   register(credentials) {
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type':  'application/json',
+        'Content-Type':  'application/json'
+       // 'Access-Control-Allow-Origin':'*'
       })
     };
 
-    this.http.post('http://localhost:8081/register', credentials, httpOptions)
+    this.http.post('http://localhost:8298/account-management/register', credentials, httpOptions)
+
     .subscribe(data => {
       console.log(data['message']);
       this.presentWarning('Account registered successfully!',
@@ -42,14 +44,16 @@ export class ClientsService {
         'Content-Type':  'application/json',
       })
     };
-
-    this.http.post('http://localhost:8081/login', credentials, httpOptions)
+   
+    this.http.post('http://localhost:8298/account-management/login', credentials, httpOptions)
     .subscribe(data => {
       console.log(data);
-      this.accessToken = data['accessToken'];
-      this.email = credentials['email'];
+      this.accessToken = data['token'];
+      this.email =  JSON.parse(credentials)['email'];
+     //  credentials['email'];
       this.loggedIn = true;
-      console.log('Acces token received:' + this.accessToken);
+      console.log('hey sunt in login');
+     // console.log('Acces token received:' + this.email);
       this.router.navigateByUrl('app/menu/home');
      }, error => {
       this.presentWarning('Atentie!', error.error['message']);
@@ -57,13 +61,33 @@ export class ClientsService {
   }
 
   async presentWarning(hd: String, msg: String) {
+   console.log('ms ul asta e ' + msg);
     const alert = await this.alertController.create({
       header: hd.toString(),
       subHeader: '',
-      message:  msg.toString(),
+      message:
+      //"" + msg,
+  msg.toString(),
       buttons: ['OK']
     });
 
     await alert.present();
   }
+
+  mypackagesdriverget()
+  {
+   console.log('Acces email MY PACK DRIVER ' + this.email);
+   //console.log('Acces token MY PACK DRIVER ' + data['email']);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization':'Bearer ' + this.accessToken
+      })  
+      };
+      
+    return this.http.get('http://localhost:8298/package-management/packages/driver/'+this.email,httpOptions)
+
+  }
+
 }
